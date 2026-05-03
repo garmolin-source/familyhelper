e22d0fe437854f9b2d04880dab43cb5451969664
garmolin-source <garmolin@gmail.com>
2026-05-03 09:30:47 +0300

    Test sheets script uses group-to-child mapping

--- scripts/test-sheets.js
+++ scripts/test-sheets.js
@@ -1,9 +1,15 @@
 require('dotenv').config();
 const { pickAndSignUp } = require('../src/sheets');
+const { getChildForGroup } = require('../src/config');
 
 const url = 'https://docs.google.com/spreadsheets/d/1wpMe6RzEaBYRckMP2dE5t8AndweGYOcDQqDEj3HNnpA/edit?usp=sharing';
-const signerName = 'כרמי';
-const eventContext = 'אירוע בית ספרי - בדיקת רישום אוטומטי';
+const groupName = process.argv[2] || 'כיתה א׳2 אלונים - הורים';
+const childInfo = getChildForGroup(groupName);
+const signerName = childInfo?.child || 'אור';
+const eventContext = `${groupName} - בדיקת רישום אוטומטי`;
+
+console.log(`Group: ${groupName}`);
+console.log(`Signing up as: ${signerName}\n`);
 
 async function run() {
   console.log('Reading sheet and picking item...\n');
